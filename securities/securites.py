3eeb30bb8c28c80025f923efd74b289473717391
Robert Prellwitz <robprellwitz@uwalumni.com>
2021-01-16 06:48:29 -0600

    added equity and option classes

---
+++ securities/securites.py
@@ -0,0 +1,12 @@
+class Equity:
+    def __init__(self, name, ticker):
+        self.name = name
+        self.ticker = ticker
+
+class Option:
+    def __init__(self, underlying, symbol, type, strike, expiration ):
+        self.underlying
+        self.symbol
+        self.type
+        self.strike
+        self.expiration
