41584ff455824b9d47804cd3baeb9efeb6c77831
Robert Prellwitz <robprellwitz@uwalumni.com>
2021-01-21 17:21:12 -0600

    adds ability to retrieve data from E*Trade generated file

--- app/securities/holdings.py
+++ app/securities/holdings.py
@@ -1,3 +1,4 @@
+import pandas
 from app.securities.securities import Equity, Security
 
 class Equity_Data:
@@ -37,7 +38,6 @@ class Security_Data:
         self.securities = {}
         self._securities_key =0
 
-
     def add_security(self, security):
         self._securities_key += 1
         self.securities[self._securities_key] = security
@@ -60,12 +60,28 @@ class Security_Data:
     def get_securities(self):
         securities = []
         for security_key, security in self.securities.items():
-            security_ = Security(security.name, security.ticker, security.shares)
+            security_ = Security(security.symbol, security.type, security.underlying, security.optiontype, security.position, security.strike, security.expiration)
             securities.append((security_key, security_))
         return securities
 
+    def get_holdings(self, csv):
+        df = pandas.read_csv(csv, engine='python', header=6, skipfooter=4)
+        # df = pandas.read_csv(csv)
+        tup = df.to_numpy()
+        return tup
 
-
+    def seed_holdings(self, holdings):
+        secdb = Security_Data()
+        for item in holdings:
+            symbol = item[0]
+            sectype = item[1]
+            underlying = item[2]
+            optiontype = item[3]
+            position = item[4]
+            strike = item[5]
+            expiration = item[6]
+            secdb.add_security(Security(symbol, sectype, underlying, optiontype, position, strike, expiration))
+        return secdb
 
 
 
